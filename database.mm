e16c1f897cf95abfe1981de6d11f552c707cc119
krigejd <charlotte.krige@proton.me>
2026-01-23 21:18:36 +0200

    Theorems up until mp1i

--- database.mm
+++ database.mm
@@ -2,7 +2,7 @@ $( My personal theorem database in metamath $)
 
 $( 
     ------------------------
-    Declaration of constants 
+    Declarations of constants 
     ------------------------
 $)
 
@@ -17,7 +17,7 @@ $c -. $.
 
 $( 
     ------------------------
-    Declaration of variables 
+    Declarations of variables 
     ------------------------
 $)
 
@@ -29,7 +29,7 @@ $v th $.
 
 $( 
     -----------------------------
-    Declaration of variable types 
+    Declarations of variable types 
     -----------------------------
 $)
 
@@ -41,9 +41,77 @@ wth $f wff th $.
 
 $( 
     --------------------------------------------
-    Recursive definition of well-formed formulas 
+    Recursive definitions of well-formed formulas 
     --------------------------------------------
 $)
 
-wi $a wff ph -> ps $.
+wi $a wff ( ph -> ps ) $.
 wn $a wff -. ph $.
+
+$( 
+    --------------------------------
+    Axioms of Propositional Calculus
+    --------------------------------
+$)
+
+ax-1 $a |- ( ph -> ( ps -> ph ) ) $.
+ax-2 $a |- ( ( ph -> ( ps -> ch ) ) -> ( ( ph -> ps ) -> ( ph -> ch ) ) ) $.
+ax-3 $a |- ( ( -. ps -> -. ph ) -> ( ph -> ps ) ) $.
+
+${ 
+    ax-mp.1 $e |- ph $.
+    ax-mp.2 $e |- ( ph -> ps ) $.
+    ax-mp $a |- ps $. 
+$}
+
+$( 
+    ----------------------------------
+    Theorems of Propositional Calculus    
+    ----------------------------------
+$)
+
+$( 
+    -----------------------
+    Theorems of Implication
+    -----------------------
+$)
+
+  ${
+    mp2.1 $e |- ph $.
+    mp2.2 $e |- ps $.
+    mp2.3 $e |- ( ph -> ( ps -> ch ) ) $.
+    $( A double modus ponens inference $)
+    mp2 $p |- ch $=
+      ( wi ax-mp ) BCEABCGDFHH $.
+  $}
+
+  ${
+    mp2b.1 $e |- ph $.
+    mp2b.2 $e |- ( ph -> ps ) $.
+    mp2b.3 $e |- ( ps -> ch ) $.
+    $( A double modus ponens inference $)
+    mp2b $p |- ch $=
+      ( ax-mp ) BCABDEGFG $.
+  $}
+
+  ${
+    a1i.1 $e |- ph $.
+    $( Inference introducing an antecedent $)
+    a1i $p |- ( ps -> ph ) $=
+      ( wi ax-1 ax-mp ) ABADCABEF $.
+  $}
+
+  ${
+    2a1i.1 $e |- ph $.
+    $( Inference introducing two antecedents $)
+    2a1i $p |- ( ch -> ( ps -> ph ) ) $=
+      ( wi a1i ) BAECABDFF $.
+  $}
+
+  ${
+    mp1i.1 $e |- ph $.
+    mp1i.2 $e |- ( ph -> ps ) $.
+    $( Inference detaching an antecedent and introducing a new one $)
+    mp1i $p |- ( ch -> ps ) $=
+      ( ax-mp a1i ) BCABDEFG $.
+  $}
